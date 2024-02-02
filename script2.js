05e94223f405f600dd3852eaa65eaab468652a00
@patriciobe <patriciovaldez944@gmail.com>
2024-02-02 18:17:16 -0500

    ddd

--- script2.js
+++ script2.js
@@ -18,7 +18,7 @@ popupWhatsApp = () => {
   let msg = document.getElementById('whats-in').value;
   let relmsg = msg.replace(/ /g,"%20");
      
-   window.open('https://wa.me/593980292129?text='+relmsg, '_blank'); 
+   window.open('https://wa.me/+593981220327?text='+relmsg, '_blank'); 
   
   });
 
